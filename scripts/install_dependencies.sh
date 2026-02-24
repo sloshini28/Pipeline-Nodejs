a3221149ff8593212a533658b26f168203730dfa
sloshini28 <loshini.sankaran2004@gmail.com>
2026-02-24 17:52:53 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+# Go to backend and install npm packages
+cd /home/ec2-user/student-system/backend
+npm install
 
+# Install PM2 globally so we can run the apps in the background
+sudo npm install -g pm2
